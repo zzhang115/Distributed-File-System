83509be6d56e3fd5470ad1fac286812a24b961ad
zzhang115 <zzhang115@dons.usfca.edu>
2017-09-16 16:45:27 -0700

    break chunks done

--- src/main/java/edu/usfca/cs/dfs/Client.java
+++ src/main/java/edu/usfca/cs/dfs/Client.java
@@ -2,7 +2,7 @@ package edu.usfca.cs.dfs;
 
 
 import java.io.*;
-import java.net.Socket;
+import java.nio.file.Files;
 import java.util.logging.Logger;
 
 public class Client {
@@ -24,19 +24,27 @@ public class Client {
     public static void breakFiletoChunks(File file) throws IOException {
         int sizeOfChunk = 1024; // 1kB per chunk
         byte[] buffer = new byte[sizeOfChunk];
-        String filename = file.getName();
+        String fileName = file.getName();
         try (FileInputStream fileInputStream = new FileInputStream(file);
              BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
             int bytesAmount = 0;
             int chunksId = 1;
             while ((bytesAmount = bufferedInputStream.read(buffer)) > 0) {
-                String fileChunkName = String.format("%s.%05d", filename, chunksId++);
+                String fileChunkName = String.format("%s.%05d", fileName, chunksId++);
                 File chunk = new File(chunkPath, fileChunkName);
+                removeChunksIfExist(chunk);
                 try (FileOutputStream out = new FileOutputStream(chunk)) {
                     out.write(buffer, 0, bytesAmount);
                 }
             }
         }
+    }
 
+    public static void removeChunksIfExist(File chunk) throws IOException {
+        if (chunk.exists()) {
+            logger.info("Client: Chunk " + chunk.getName() + " already exists, removing.");
+            Files.delete(chunk.toPath());
+            logger.info("Client: Chunk " + chunk.getName() + " removed.");
+        }
     }
 }
