2a12d64c30f00260d395ce7dba5f53b62bc33c6b
zzhang115 <zzhang115@dons.usfca.edu>
2017-09-16 15:55:50 -0700

    initial commit

--- src/main/java/edu/usfca/cs/dfs/Client.java
+++ src/main/java/edu/usfca/cs/dfs/Client.java
@@ -1,10 +1,19 @@
 package edu.usfca.cs.dfs;
 
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Logger;
+
 public class Client {
+    private static Logger logger = Logger.getLogger("log");
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws IOException {
+        logger.info("first log info");
     }
 
+    public static void init() throws IOException {
+        Socket socket = new Socket("localhost", 8080);
+
+    }
 }
