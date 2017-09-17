96e814844975cba1addc644eca42962c41dcc774
zzhang115 <zzhang115@dons.usfca.edu>
2017-09-16 18:42:05 -0700

    break chunks done

--- src/main/java/edu/usfca/cs/dfs/Client.java
+++ src/main/java/edu/usfca/cs/dfs/Client.java
@@ -22,17 +22,35 @@ public class Client {
     }
 
     public static void clientInit() throws IOException {
-//      socket = new Socket("localhost", 8080);
         breakFiletoChunks(new File(filePath));
+        File chunks = new File(chunkPath);
+        for (File chunk : chunks.listFiles()) {
+//            System.out.println(chunkPath + "/" + chunk.getName());
+            sendRequestToController(chunk.getName());
+        }
     }
 
-    public static void sendRequestToController() throws IOException {
-        ByteString data = ByteString.copyFromUtf8("Hello World!");
+    public static void sendRequestToController(String chunkName) throws IOException {
+        if (chunkName == null || !chunkName.contains(".")) {
+            return;
+        }
+        String[] nameStrArray = chunkName.split("\\.");
+        int chunkId = Integer.parseInt(nameStrArray[nameStrArray.length - 1]);
+
+        socket = new Socket("localhost", 8080);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(chunkPath + "/" + chunkName));
+        String line;
+        StringBuffer stringBuffer = new StringBuffer();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuffer.append(line);
+        }
+
+        ByteString data = ByteString.copyFromUtf8(stringBuffer.toString());
 
         StorageMessages.StoreChunk storeChunkMsg
                 = StorageMessages.StoreChunk.newBuilder()
-                .setFileName("my_file.txt")
-                .setChunkId(3)
+                .setFileName(chunkName)
+                .setChunkId(chunkId)
                 .setData(data)
                 .build();
 
@@ -41,9 +59,9 @@ public class Client {
                         .setStoreChunkMsg(storeChunkMsg)
                         .build();
 
-            msgWrapper.writeDelimitedTo(socket.getOutputStream());
+        msgWrapper.writeDelimitedTo(socket.getOutputStream());
 
-            socket.close();
+        socket.close();
     }
 
     public static void breakFiletoChunks(File file) throws IOException {
@@ -55,7 +73,7 @@ public class Client {
             int bytesAmount = 0;
             int chunksId = 1;
             while ((bytesAmount = bufferedInputStream.read(buffer)) > 0) {
-                String fileChunkName = String.format("%s.%05d", fileName, chunksId++);
+                String fileChunkName = String.format("%s.%03d", fileName, chunksId++);
                 File chunk = new File(chunkPath, fileChunkName);
                 removeChunksIfExist(chunk);
                 try (FileOutputStream out = new FileOutputStream(chunk)) {
--- src/main/java/edu/usfca/cs/dfs/StorageNode.java
+++ src/main/java/edu/usfca/cs/dfs/StorageNode.java
@@ -1,14 +1,43 @@
 package edu.usfca.cs.dfs;
 
+import com.google.protobuf.ByteString;
+
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class StorageNode {
-
+    private static ServerSocket serverSocket;
     public static void main(String[] args) 
     throws Exception {
         String hostname = getHostname();
         System.out.println("Starting storage node on " + hostname + "...");
+        storageNodeInit();
+    }
+
+    public static void storageNodeInit() throws IOException {
+
+    serverSocket = new ServerSocket(8080);
+        System.out.println("Listening...");
+        while (true) {
+            Socket socket = serverSocket.accept();
+            StorageMessages.StorageMessageWrapper msgWrapper
+                = StorageMessages.StorageMessageWrapper.parseDelimitedFrom(
+                        socket.getInputStream());
+
+            if (msgWrapper.hasStoreChunkMsg()) {
+                StorageMessages.StoreChunk storeChunkMsg
+                    = msgWrapper.getStoreChunkMsg();
+                System.out.println("Storing file name: "
+                        + storeChunkMsg.getFileName());
+                System.out.println("Storing file ID: " + storeChunkMsg.getChunkId());
+                ByteString data = storeChunkMsg.getData();
+                String dataStr = data.toStringUtf8();
+                System.out.println("Storing file data: " + dataStr);
+            }
+        }
     }
 
     /**
