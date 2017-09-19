fcb29d3bfa6e3d6ee0c79898f9c40727ef697223
zzhang115 <zzhang115@dons.usfca.edu>
2017-09-18 21:54:12 -0700

    add chunks files,

--- src/main/java/edu/usfca/cs/dfs/Controller.java
+++ src/main/java/edu/usfca/cs/dfs/Controller.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 
 public class Controller {
 
-    private ArrayList<StorageNode> storageNodes;
-    private ArrayList<DFSFile> files;
+    private static ArrayList<StorageNode> storageNodes;
+    private static ArrayList<DFSFile> files;
     public static void main(String[] args) {
 
         System.out.println("Starting controller...");
 
 
+    }
+
+    public static void controllerInit() {
+        storageNodes = new ArrayList<StorageNode>();
+        files = new ArrayList<DFSFile>();
     }
 
 }
---
+++ src/main/java/edu/usfca/cs/dfs/HeartBeat.java
@@ -0,0 +1,18 @@
+package edu.usfca.cs.dfs;
+
+import java.util.HashMap;
+
+/**
+ * Created by zzc on 9/18/17.
+ */
+public class HeartBeat {
+    public int storageNodeID;
+    public HashMap<String, String> metaMap;
+    public double freeSpace;
+
+    public HeartBeat(int storageNodeID, double freeSpace) {
+        this.storageNodeID = storageNodeID;
+        this.metaMap = new HashMap<String, String>();
+        this.freeSpace = freeSpace;
+    }
+}
