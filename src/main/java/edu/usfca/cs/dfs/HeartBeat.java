ed5b3d1693006b1596134d16326e8db7d3a9a15c
zzhang115 <zzhang115@dons.usfca.edu>
2017-09-22 22:51:30 -0700

    add chunks files,

--- src/main/java/edu/usfca/cs/dfs/HeartBeat.java
+++ src/main/java/edu/usfca/cs/dfs/HeartBeat.java
@@ -1,7 +1,6 @@
 package edu.usfca.cs.dfs;
 
 import java.util.HashMap;
-
 /**
  * Created by zzc on 9/18/17.
  */
@@ -9,6 +8,7 @@ public class HeartBeat {
     public int storageNodeID;
     public HashMap<String, String> metaMap;
     public double freeSpace;
+//    public timestamp;
 
     public HeartBeat(int storageNodeID, double freeSpace) {
         this.storageNodeID = storageNodeID;
